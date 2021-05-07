c8ed85332ee99d40e471592a6f876ae946fd7082
Blessing <bntoko@gmail.com>
2021-05-07 11:27:36 +0200

    install passport, passport-local, passport-local-mongoose; configured passport

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const expressSession = require('express-session');
+const passport = require('passport');
+const passLocalMong = require('passport-local-mongoose');
 
 const app = express();
 const port = process.env.PORT || 4177;
@@ -15,12 +17,21 @@ app.use(express.urlencoded({
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+app.use(expressSession({
+    secret: 'This is my secret.',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 // SECTION: Database & Schema
 
 mongoose.connect('mongodb://localhost:27017/userDB', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useCreateIndex: true
 }, () => {
     console.log('Connected to MongoDB succesfully');
 });
@@ -36,7 +47,13 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+userSchema.plugin(passLocalMong);
+
 const User = mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // SECTION: Get routes
 
@@ -55,65 +72,83 @@ app.get('/register', (req, res) => {
 // SECTION: Post routes
 
 app.post('/register', (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
 
-    bcrypt.hash(password, saltRounds, (err, hash) => {
-        User.findOne({
-            email: username
-        }, (err, result) => {
-            if (!err) {
-                if (result) {
-                    res.send('Email already exists');
-                } else {
-                    const newUser = new User({
-                        email: username,
-                        password: hash
-                    });
-
-                    newUser.save(err => {
-                        if (!err) {
-                            console.log('User registered successfully');
-                            res.render('secrets');
-                        } else {
-                            console.log(err);
-                        }
-                    });
-                }
-            } else {
-                console.log('Error in register route ->', err);
-            }
-        });
-    });
 
 });
 
 app.post('/login', (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
 
-    User.findOne({
-        email: username
-    }, (err, data) => {
-        if (!err) {
-            if (data) {
-                bcrypt.compare(password, data.password, (err, result) => {
-                    if (result) {
-                        res.render('secrets');
-                    } else {
-                        res.send('Password entered is incorrect, please try again');
-                    }
-                });
-            } else {
-                console.log('User not found');
-            }
-        } else {
-            res.send(err);
-        }
-    });
 });
 
 // SECTION: Listening
 app.listen(port, () => {
     console.log('Server started successfully');
 });
+
+
+
+
+
+
+
+
+
+
+// app.post('/register', (req, res) => {
+//     const username = req.body.username;
+//     const password = req.body.password;
+
+//     bcrypt.hash(password, saltRounds, (err, hash) => {
+//         User.findOne({
+//             email: username
+//         }, (err, result) => {
+//             if (!err) {
+//                 if (result) {
+//                     res.send('Email already exists');
+//                 } else {
+//                     const newUser = new User({
+//                         email: username,
+//                         password: hash
+//                     });
+
+//                     newUser.save(err => {
+//                         if (!err) {
+//                             console.log('User registered successfully');
+//                             res.render('secrets');
+//                         } else {
+//                             console.log(err);
+//                         }
+//                     });
+//                 }
+//             } else {
+//                 console.log('Error in register route ->', err);
+//             }
+//         });
+//     });
+
+// });
+
+// app.post('/login', (req, res) => {
+//     const username = req.body.username;
+//     const password = req.body.password;
+
+//     User.findOne({
+//         email: username
+//     }, (err, data) => {
+//         if (!err) {
+//             if (data) {
+//                 bcrypt.compare(password, data.password, (err, result) => {
+//                     if (result) {
+//                         res.render('secrets');
+//                     } else {
+//                         res.send('Password entered is incorrect, please try again');
+//                     }
+//                 });
+//             } else {
+//                 console.log('User not found');
+//             }
+//         } else {
+//             res.send(err);
+//         }
+//     });
+// });
